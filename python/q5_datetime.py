660e889276e5119eca8ce156a7c13c3305252d31
Diana Lam <diana@Dianas-MBP.home>
2015-12-19 15:26:24 -0500

    added solutions

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -4,10 +4,39 @@
 date_start = '01-02-2013'  
 date_stop = '07-28-2015'   
 
+# a) solution
+date_format = '%m-%d-%Y' # create date format to match given start/stop format
+
+start = datetime.strptime(date_start, date_format) # change string to datetime object
+stop = datetime.strptime(date_stop, date_format)
+
+dif = stop - start
+print dif
+
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015' 
+
+# b) solution
+
+date_format_b = '%m%d%Y'
+
+start = datetime.strptime(date_start, date_format_b)
+stop = datetime.strptime(date_stop, date_format_b)
+
+difb = stop - start
+print difb 
 
 ####c)  
 date_start = '15-Jan-1994'  
 date_stop = '14-Jul-2015'  
+
+# c) solution
+
+date_format_c = '%d-%b-%Y'
+
+start = datetime.strptime(date_start, date_format_c)
+stop = datetime.strptime(date_stop, date_format_c)
+
+difc = stop - start
+print difc
--- python/q6_strings.py
+++ python/q6_strings.py
@@ -18,7 +18,10 @@ def donuts(count):
     >>> donuts(99)
     'Number of donuts: many'
     """
-    raise NotImplementedError
+    if count >= 10:
+        return 'Number of donuts: many'
+    else:
+        return 'Number of donuts:', count
 
 
 def both_ends(s):
@@ -37,7 +40,10 @@ def both_ends(s):
     >>> both_ends('xyz')
     'xyyz'
     """
-    raise NotImplementedError
+    if len(s) > 2:
+        return s[:2] + s[-2:]
+    else:
+        return ''
 
 
 def fix_start(s):
@@ -56,7 +62,10 @@ def fix_start(s):
     >>> fix_start('donut')
     'donut'
     """
-    raise NotImplementedError
+    new_string = s[0] # make new string with first letter of given string
+    for i in s[1:len(s)]: # traverse through s, starting with 2nd letter
+        new_string += i.replace(s[0], '*') # and replace letters that match the first letter with '*'
+    return new_string
 
 
 def mix_up(a, b):
@@ -74,7 +83,9 @@ def mix_up(a, b):
     >>> mix_up('pezzy', 'firm')
     'fizzy perm'
     """
-    raise NotImplementedError
+    new_a = b[:2] + a[2:]
+    new_b = a[:2] + b[2:]
+    return new_a + ' ' + new_b
 
 
 def verbing(s):
@@ -91,7 +102,13 @@ def verbing(s):
     >>> verbing('do')
     'do'
     """
-    raise NotImplementedError
+    if len(s) >= 3:
+        if s[-3:] == 'ing':
+            return s + 'ly'
+        else:
+            return s + 'ing'
+    else:
+        return s
 
 
 def not_bad(s):
@@ -111,7 +128,13 @@ def not_bad(s):
     >>> not_bad("It's bad yet not")
     "It's bad yet not"
     """
-    raise NotImplementedError
+    pos_n = s.find('not') # find 'not' in string and store index
+    pos_b = s.find('bad') 
+    if pos_b > pos_n:
+        new_string = s.replace(s[pos_n:pos_b+len('bad')], 'good') # replace slice of string from start of 'not' to end of 'bad' with 'good'
+        return new_string
+    else:
+        return s
 
 
 def front_back(a, b):
@@ -130,4 +153,27 @@ def front_back(a, b):
     >>> front_back('Kitten', 'Donut')
     'KitDontenut'
     """
-    raise NotImplementedError
+    # slice up a
+    if len(a) % 2 == 0:
+        afront = a[:len(a)/2]
+        aback = a[len(a)/2:]
+    else:
+        afront = a[:len(a)/2+1]
+        aback = a[len(a)/2+1:]
+    
+    # slice up b
+    if len(b) % 2 == 0:
+        bfront = b[:len(b)/2]
+        bback = b[len(b)/2:]
+    else:
+        bfront = b[:len(b)/2+1]
+        bback = b[len(b)/2+1:]
+    
+    # return compiled string
+    return afront + bfront + aback + bback
+
+    '''
+    @ reshama: is there a better way to write this last one that doesn't require repeating the same if/else test on a and b?
+    I tried writing another function within the function and calling it on each, but I couldn't run it on both a and b
+    because 'return' would end the function.
+    '''
