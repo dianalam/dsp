f5fad535af31403c759487445501471f7757adff
Diana Lam <diana@Dianas-MBP.home>
2015-12-21 21:46:18 -0500

    answered csv question and updated emails.csv

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,17 @@
-PLACE YOUR CODE HERE
+import csv
+
+# open and parse list of emails
+with open ('faculty.csv') as faculty:
+    emails = []
+    facDict = csv.DictReader(faculty)
+    for row in facDict:
+        emails.append(row[' email'])
+
+# write emails to new csv 
+newcsv = open ('emails.csv', 'wb')
+mywriter = csv.writer(newcsv)
+
+for item in emails:
+	mywriter.writerow([item])
+
+newcsv.close()
